732223fd5ce51a2fcd94d585357cd592b4de0a46
Sudhanshu Ranjan <131840982+sudhanshuranjannn@users.noreply.github.com>
2025-10-07 11:26:39 +0530

    Update settings.py

--- bitcoin_sip/settings.py
+++ bitcoin_sip/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # -------------------------------------------------------------------
 # Static Files
 # -------------------------------------------------------------------
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
